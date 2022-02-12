3ae0e5747c7af511caae1845d2ee0bd0cfc430df
Surely Soto C <ssotoc14@gmail.com>
2022-02-12 18:18:53 -0500

    Mi sesion 5

---
+++ JS5/Continue on JS.js
@@ -0,0 +1,28 @@
+for (let timer = 0; timer <= 10; timer++){
+    if (timer % 2 !== 0){
+        continue;
+    }
+    console.log(timer);
+    
+}
+//ejercicio random de alex
+let number = 23
+    if (number % 2 == 0){
+        console.log( "numero par" )
+    }
+    else {
+        console.log("incorrecto")
+    }
+
+//No entendi esta
+
+//ETIQUETA O LABEL
+/*
+inicio
+for (timer = 0; timer <= 10; timer++){
+    if (timer % 2 !== 0){
+        continue inicio;
+    }
+    console.log(timer);
+} 
+*/
